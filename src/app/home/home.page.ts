bb68cb56882672195ac126679a1bfc42bf659712
NeumaieD <neumaier-dominik@web.de>
2023-08-15 00:05:01 +0200

    - Listenansicht erstellt - QR-Code von Picture-Funktion implementiert - Horizontale Elipse Menu implemenitert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
-import { AlertController } from '@ionic/angular';
+import { Barcode, BarcodeFormat, BarcodeScanner, ReadBarcodesFromImageOptions } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController, ActionSheetController } from '@ionic/angular';
+import { Share } from '@capacitor/share';
+import { Clipboard } from '@capacitor/clipboard';
+
+
+import { FilePicker } from '@capawesome/capacitor-file-picker';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage implements OnInit {
   isSupported = false;
   barcodes: Barcode[] = [];
 
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController, private actionSheetController: ActionSheetController) {}
 
   ngOnInit() {
     BarcodeScanner.isSupported().then((result) => {
@@ -19,7 +25,45 @@ export class HomePage implements OnInit {
     });
   }
 
+  async pickImage(): Promise<void> {
+
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+
+    const pickImage = async () => {
+      const { files } = await FilePicker.pickImages({
+        multiple: true,
+      });
+      return files[0];
+    };
+
+    const pickedImage = await pickImage();
+    const { barcodes } = await BarcodeScanner.readBarcodesFromImage({
+      formats: [BarcodeFormat.QrCode],
+      path: pickedImage.path!,
+    });
+
+    console.log("Test");
+    console.log(barcodes.length);
+
+    if(barcodes.length == 0){
+      const alert = await this.alertController.create({
+        header: 'QR-Code Nix gut diese',
+        message: 'Mies. Du Noob. Nix QR-Code. Mach neu',
+        buttons: ['Vallah Kriese'],
+      });
+      await alert.present();
+    } else {
+      this.barcodes.push(...barcodes);
+    }
+
+  }
+
   async scan(): Promise<void> {
+
     const granted = await this.requestPermissions();
     if (!granted) {
       this.presentAlert();
@@ -27,6 +71,9 @@ export class HomePage implements OnInit {
     }
     const { barcodes } = await BarcodeScanner.scan();
     this.barcodes.push(...barcodes);
+    console.error("Erwin,Domi, Fabi");
+    console.error(barcodes.length);
+
   }
 
   async requestPermissions(): Promise<boolean> {
@@ -41,5 +88,28 @@ export class HomePage implements OnInit {
       buttons: ['OK'],
     });
     await alert.present();
+  }
+
+
+  async openActionSheet(option: any) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Options',
+      buttons: [
+        {
+          text: option.text,
+          handler: option.handler
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    await actionSheet.present();
+  }
+
+  handleOptionClick(optionText: string) {
+    console.log(optionText + ' clicked');
   }
 }
